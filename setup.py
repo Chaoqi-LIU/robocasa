1e77e2d4b04b5beb91ec1138a28876d41450a92c
Chaoqi Liu <liuchaoqi730@gmail.com>
2026-05-22 03:20:33 -0500

    Prepare praxis-robocasa package release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ lines = [x for x in lines if ".png" not in x]
 long_description = "".join(lines)
 
 setup(
-    name="robocasa",
+    name="praxis-robocasa",
     packages=[package for package in find_packages() if package.startswith("robocasa")],
     install_requires=[
         "numpy==2.2.5",
@@ -31,14 +31,21 @@ setup(
         "lxml",
         "hidapi",
         "gymnasium",
+        "praxis-robosuite>=1.5.2,<1.6",
     ],
     eager_resources=["*"],
     include_package_data=True,
     python_requires=">=3",
     description="RoboCasa365: A Large-Scale Simulation Framework for Training and Benchmarking Generalist Robots",
     author="Soroush Nasiriany, Sepehr Nasiriany, Abhiram Maddukuri, Yuke Zhu",
-    url="https://github.com/robocasa/robocasa",
+    url="https://github.com/Chaoqi-LIU/robocasa",
     author_email="soroush@cs.utexas.edu",
+    maintainer="Chaoqi Liu",
+    maintainer_email="liuchaoqi730@gmail.com",
+    project_urls={
+        "Source": "https://github.com/Chaoqi-LIU/robocasa",
+        "Maintainer Website": "https://chaoqi-liu.com",
+    },
     version="1.0.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
